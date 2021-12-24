25f94f4db0729a0aa61e00ad6a622891c84bd967
Shin-sibainu <shincode0712@gmail.com>
2021-12-24 10:30:06 +0900

    mod

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import * as THREE from "three";
 import * as dat from "lil-gui";
 
 /**
- * Debug
+ * デバッグ(色つけるときに追加)
  */
 const gui = new dat.GUI();
 
@@ -17,16 +17,46 @@ gui.addColor(parameters, "materialColor").onChange(() => {
 });
 
 /**
- * Base
+ * 必須の3要素
  */
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
 
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
 // Scene
 const scene = new THREE.Scene();
 
+//Camera
+
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  35,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.z = 6;
+scene.add(camera);
+
 /**
- * Objects
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  alpha: true,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * オブジェクトを作成しよう
  */
 //material
 const material = new THREE.MeshPhysicalMaterial({
@@ -40,7 +70,6 @@ gui.add(material, "metalness").min(0).max(1).step(0.001);
 gui.add(material, "roughness").min(0).max(1).step(0.001);
 
 // Meshes
-const objectDistance = 4;
 const mesh1 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.4, 16, 60), material);
 const mesh2 = new THREE.Mesh(new THREE.OctahedronGeometry(), material);
 const mesh3 = new THREE.Mesh(
@@ -93,14 +122,7 @@ const directionalLight = new THREE.DirectionalLight("#ffffff", 4);
 directionalLight.position.set(0.5, 1, 0);
 scene.add(directionalLight);
 
-/**
- * Sizes
- */
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-};
-
+//ブラウザのリサイズ操作
 window.addEventListener("resize", () => {
   // Update sizes
   sizes.width = window.innerWidth;
@@ -115,33 +137,6 @@ window.addEventListener("resize", () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
-/**
- * Camera
- */
-//group
-const cameraGroup = new THREE.Group();
-scene.add(cameraGroup);
-
-// Base camera
-const camera = new THREE.PerspectiveCamera(
-  35,
-  sizes.width / sizes.height,
-  0.1,
-  100
-);
-camera.position.z = 6;
-cameraGroup.add(camera);
-
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-  alpha: true,
-});
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
 //cursor
 const cursor = {};
 cursor.x = 0;
@@ -150,7 +145,6 @@ cursor.y = 0;
 window.addEventListener("mousemove", (event) => {
   cursor.x = event.clientX / sizes.width - 0.5;
   cursor.y = event.clientY / sizes.height - 0.5;
-
   // console.log(cursor);
 });
 
@@ -187,6 +181,7 @@ rot();
 const clock = new THREE.Clock();
 let previousTime = 0;
 
+//アニメーション
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
   let deltaTime = elapsedTime - previousTime;
@@ -194,14 +189,12 @@ const tick = () => {
   // console.log(deltaTime); 0.013　パソコンの性能によってデルタタイムが異なる。
 
   //camera animate
-  camera.position.y = (-scrollY / sizes.height) * objectDistance;
+  // camera.position.y = (-scrollY / sizes.height) * objectDistance;
 
-  const parallaxX = cursor.x * 0.5;
-  const parallaxY = -cursor.y * 0.5;
-  cameraGroup.position.x +=
-    (parallaxX - cameraGroup.position.x) * 5 * deltaTime;
-  cameraGroup.position.y +=
-    (parallaxY - cameraGroup.position.y) * 5 * deltaTime;
+  const parallaxX = cursor.x * 0.6;
+  const parallaxY = -cursor.y * 0.6;
+  camera.position.x += (parallaxX - camera.position.x) * 5 * deltaTime;
+  camera.position.y += (parallaxY - camera.position.y) * 5 * deltaTime;
 
   //mesh aniamte
   for (const mesh of sectionMeshes) {
